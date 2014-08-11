b51f75575044a61197464985bf9d996003f9254b
bankpan <1075307203@qq.com>
2014-08-12 00:08:13 +0800

    初始版
    初始版本

---
+++ 1.bat
@@ -0,0 +1,3 @@
+rsync -v -rlt -z  --delete --password-file="/cygdrive/C/secret.txt" -p -o -g --progress "/cygdrive/C/admin1.yxb" "pyh@192.168.1.168::test/
+pause
+
---
+++ Debug/1.bat
@@ -0,0 +1,3 @@
+rsync --exclude='/*/.local' --no-owner --no-group  -v -rlt -z  --progress --delete --password-file="secret.txt" -p -o -g "/cygdrive/C/mynormal_1.yxb" "pyh@192.168.2.168::test
+pause
+
